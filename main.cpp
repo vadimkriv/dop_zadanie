9b55ecf0dee973355cd23158675296a7b71734eb
vadimkriv <polenko1029@gmail.com>
2024-05-29 22:34:28 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    for (int i = 0; i < num; ++i) {
+        std::cout << "I will never loose deadline!" << std::endl;
+    }
+    return 0;
+}
